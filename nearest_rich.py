61eea86ce3ac0ecec07c113eba6f824ec9d00b61
IT4211 <rhdqor100@live.co.kr>
2017-04-04 11:48:09 +0900

    Update nearest_rich.py

--- nearest_rich.py
+++ nearest_rich.py
@@ -22,13 +22,19 @@ except AttributeError:
 class NearestRich(QtGui.QMainWindow):
 
     def __init__(self, parent = None):
+
         QtGui.QWidget.__init__(self, parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.actionOpen.triggered.connect(self._Open)
         self.ui.actionOpen_Dir.triggered.connect(self._OpenDir)
         self.ui.actionStart_Analysis.triggered.connect(self._StartAnalysis)
-        self.ui.listWidget_file.itemDoubleClicked.connect(self.ItemClicked)
+        self.header = self.ui.tableWidget_file.horizontalHeader()
+        self.header.setResizeMode(1, QtGui.QHeaderView.ResizeToContents)
+        self.header.setResizeMode(2, QtGui.QHeaderView.ResizeToContents)
+        self.header.setResizeMode(3, QtGui.QHeaderView.ResizeToContents)
+        self.ui.tableWidget_file.cellDoubleClicked.connect(self.ItemClicked)
+        # TODO: dock을 실수로 꺼버린 경우 다시 켤 수 있도록 View 메뉴 만들 것!
 
     def _DrawChart(self):
         pass
@@ -47,8 +53,12 @@ class NearestRich(QtGui.QMainWindow):
         if self.directory is None:
             return
         fileNames = os.listdir(self.directory)
-        # 이전에 있던 목록을 지움
-        self.ui.listWidget_file.clear()
+        # 테이블 비우기
+        rowCount = self.ui.tableWidget_file.rowCount()
+        while rowCount:
+            self.ui.tableWidget_file.removeRow(rowCount - 1)
+            rowCount -= 1
+
         for fileName in fileNames:
             filePath = os.path.join(str(self.directory), fileName)
 
@@ -56,29 +66,34 @@ class NearestRich(QtGui.QMainWindow):
             # 파일 목록 중 디렉토리가 존재하는 경우 continue
             if os.path.isdir(filePath):
                 continue
-
             try:
                 fp = open(filePath, 'r')
                 mzSignature = fp.read(2)
 
                 if mzSignature == 'MZ': # 0x4D5A
-                    self.ui.listWidget_file.addItem(fileName)
+                    self.rowPosition = self.ui.tableWidget_file.rowCount()
+                    self.ui.tableWidget_file.insertRow(self.rowPosition)
+                    self.ui.tableWidget_file.setItem(self.rowPosition, 0, QtGui.QTableWidgetItem(_fromUtf8(fileName)))
+                    self.ui.tableWidget_file.setItem(self.rowPosition, 2, QtGui.QTableWidgetItem(_fromUtf8(filePath)))
                 else:
                     pass
             except:
-                oLog.writeLog("ERROR", "File Read Failure: " + fileName)
+                self.ui.textEdit_status.insertPlainText(_fromUtf8(fileName + " 파일을 읽는 도중 오류가 발생하였습니다.\n"))
                 exit(0)
             else:
                 fp.close()
-        fileCount = self.ui.listWidget_file.count()
+        fileCount = self.ui.tableWidget_file.rowCount()
         self.ui.textEdit_status.insertPlainText(_fromUtf8("분석 대상 파일 " + str(fileCount) + "개를 모두 읽어들였습니다.\n"))
 
     def _StartAnalysis(self):
         pass
 
-    def ItemClicked(self, item):
-        print item.text()
-        pass
+    @QtCore.pyqtSlot(int, int)
+    def ItemClicked(self, row, col):
+
+        itemPath = self.ui.tableWidget_file.item(row, 3)
+        # 파일 오픈해서 Rich Header 파싱 후
+        # 새로운 Dialog에서 출력
 
     def _WarningMessage(self, msg):
         print "[Warning] ", msg
@@ -94,7 +109,6 @@ if __name__ == "__main__":
 
     logPath = os.path.join(os.getcwd(), "nearest_rich.log")
     oLog = ForensicLog(logPath)
-
     oLog.writeLog("INFO", "Nearest Rich Started")
 
     app = QtGui.QApplication(sys.argv)
